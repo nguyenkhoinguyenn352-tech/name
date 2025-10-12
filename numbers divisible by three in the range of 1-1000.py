e70392f074c78938aa241ea0163b3de8a167c82a
nguyenkhoinguyenn352-tech <nguyenkhoinguyenn352@gmail.com>
2025-10-12 19:59:46 +0700

    Create numbers divisible by three in the range of 1-1000.py

---
+++ numbers divisible by three in the range of 1-1000.py
@@ -0,0 +1,10 @@
+# Initialize the starting number
+number = 1
+
+# Use a while loop to go through numbers 1 to 1000
+while number <= 1000:
+    # Check if the number is divisible by 3
+    if number % 3 == 0:
+        print(number)
+    # Move to the next number
+    number += 1
